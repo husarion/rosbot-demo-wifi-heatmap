f926204c989fa4d3963a96cdf9aeb192fb6a0952
sdudiak <staszek.dudiak@irpark.pl>
2022-03-29 22:42:15 +0200

    added read_rssi.cpp

---
+++ nav2_read_rssi_at_waypoint/src/read_rssi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int read_rssi(){
+    std::fstream rssi_file;
+    rssi_file.open("/proc/net/wireless",std::ios::in);
+    if (rssi_file.is_open()){
+        std::string str;
+        for(int i = 0;i < 2;i++){
+            std::getline(rssi_file,str);
+        }
+        std::getline(rssi_file,str);
+        str = str.substr(20,3);
+        rssi_file.close();
+        return stoi(str);
+    } 
+    return 0;  
+}
