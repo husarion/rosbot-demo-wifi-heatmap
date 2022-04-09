78b8548e05c94ab090cf8234b540c637f4794d3c
sdudiak-husarion <stanislaw.dudiak@husarion.com>
2022-04-09 06:28:47 -0700

    fixed reading rssi from string and added on_activate method to lifecycle publisher

--- nav2_read_rssi_at_waypoint/src/read_rssi.cpp
+++ nav2_read_rssi_at_waypoint/src/read_rssi.cpp
@@ -3,14 +3,14 @@
 
 int read_rssi(){
     std::fstream rssi_file;
-    rssi_file.open("/proc/net/wireless",std::ios::in);
+    rssi_file.open("/fakenet_/wireless",std::ios::in);
     if (rssi_file.is_open()){
         std::string str;
         for(int i = 0;i < 2;i++){
             std::getline(rssi_file,str);
         }
         std::getline(rssi_file,str);
-        str = str.substr(20,3);
+        str = str.substr(29,3);
         rssi_file.close();
         return stoi(str);
     } 
