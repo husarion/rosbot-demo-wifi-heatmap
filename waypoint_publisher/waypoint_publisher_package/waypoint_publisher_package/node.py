61936715ecf9be40cef0bf61bfd568a550be4797
sdudiak <staszek.dudiak@irpark.pl>
2022-04-10 21:36:40 +0200

    added comments

--- waypoint_publisher/waypoint_publisher_package/waypoint_publisher_package/node.py
+++ waypoint_publisher/waypoint_publisher_package/waypoint_publisher_package/node.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from rclpy.action import ActionClient
 from nav2_msgs.action import FollowWaypoints
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Bool
 #Utils
 import tkinter
 import matplotlib.pyplot as plt
@@ -11,11 +12,11 @@ import cv2
 import yaml
 from collections import namedtuple
 from multiprocessing import Process
-from std_msgs.msg import Bool
 
 # Function displaying map of waypoints
 def show_map(map):
     plt.imshow(map)
+    plt.axis('off')
     plt.show()
 
 #Named tuple representing a single waypoint
@@ -24,9 +25,12 @@ Waypoint = namedtuple('Waypoint','x y')
 class FollowWaypointsClient(Node):
     def __init__(self):
         super().__init__('navigate_through_poses_client')
+#Create action client
         self._action_client = ActionClient(self,FollowWaypoints,'/follow_waypoints')
+#Subscribe to topic sending trigger message
         self.publisher = self.create_publisher(Bool,'/heatmap_generator_trigger',1)
-        self.p = Process(target=show_map,args=(self.map,)) #Show valid waypoints in different process (app doesn't block)
+#Show valid waypoints in different process (app doesn't block)
+        self.p = Process(target=show_map,args=(self.map,))
 #User params:
         self.declare_parameter('density',5)
         self.declare_parameter('collision_range',3)
@@ -42,11 +46,12 @@ class FollowWaypointsClient(Node):
         self.waypoint_array = []
         self.robot_frame_waypoint_array = []
 
+#Send created waypoints to FollowWaypoints action server
     def send_goal(self):
+#Generate waypoints based on saved map and user params
         self.set_waypoints()
         msg = FollowWaypoints.Goal()
         goals = []
-        #FIX move unnecessary assignments out of the loop
         for waypoint_ in self.robot_frame_waypoint_array:
             waypoint = PoseStamped()
             waypoint.header.frame_id = 'map'
@@ -61,30 +66,31 @@ class FollowWaypointsClient(Node):
             goals.append(waypoint)
         msg.poses = goals
         print('waiting for server')
-        # Get action result
+#Send goal and wait for result
         self._action_client.wait_for_server()
         self._send_goal_future = self._action_client.send_goal_async(msg)
         self._send_goal_future.add_done_callback(self.response_callback)
-    
+
+#Get response on goal sent
     def response_callback(self,future):
         goal_handle = future.result()
-        # msg = Bool
-        # msg.data = True
-        # self.publisher.publish(msg)
-        # self.p.kill() # Kill process displaying waypoints
         self.get_logger().info("goal recieved")
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
-    
+
+#Respond to action completed
     def result_callback(self,future):
-        goal_handle = future.result()
+#Publlish trigger message
         msg = Bool
         msg.data = True
         self.publisher.publish(msg)
-        self.p.kill() # Kill process displaying waypoints
+#Kill process displaying waypoints
+        self.p.kill()
         self.get_logger().info("goal achieved")
-        rclpy.shutdown() #Kill node
+#Kill node
+        rclpy.shutdown()
 
+#Create array of waypoints based on saved map and user params
     def set_waypoints(self):
         waypoint_array = []
 # Add available waypoints based on selected density
@@ -92,35 +98,29 @@ class FollowWaypointsClient(Node):
             for j in range(0,len(self.map[0]),self.density):
                 if self.map[i][j][0] == 254:
                     waypoint_array.append(Waypoint(i,j))
-# create array of valid waypoints based on collision_range param         
+# Create array of valid waypoints based on collision_range param         
         valid_waypoint_array = [waypoint for waypoint in waypoint_array if self.check_safety(waypoint)]
-        valid_waypoint_array= valid_waypoint_array #DEBUG get only one waypoint !nodebug!
-#########DEBUG################        
+        valid_waypoint_array= valid_waypoint_array
+# Show map with valid waypoints     
         for waypoint in valid_waypoint_array:   
             self.map[waypoint.x,waypoint.y] = [0,255,0] #Mark valid waypoints
-        self.map[len(self.map) - 1][0] = [0,0,255] #DEBUG show map origin
         self.p.start() #Start process displaying waypoints
-#########DEBUG################  
-# Set proper waypoint coordinates based on map params
+
+# Create real-world waypoints based on map waypoints
         for waypoint in valid_waypoint_array:
             x = waypoint.y * self.resolution + self.origin.x
             y = (len(self.map - 1) - waypoint.x) * self.resolution + self.origin.y
             world_frame_waypoint = Waypoint(x,y)
-########DEBUG################
-            print('='*20)
-            print(waypoint.x,waypoint.y)
-            print(world_frame_waypoint.x,world_frame_waypoint.y)
-            print('='*20)
-########DEBUG################  
             self.robot_frame_waypoint_array.append(world_frame_waypoint)
 
-    def check_safety(self,waypoint:Waypoint): #method for checking if selected waypoint is close to occupied or unknown space
+# Check if waypoint is not too close to occupied or unknown space
+    def check_safety(self,waypoint:Waypoint):
         xbegin,xend = waypoint.x - self.collision_range,waypoint.x + self.collision_range
         ybegin,yend = waypoint.y - self.collision_range,waypoint.y + self.collision_range
         for i in range(xbegin,xend + 1):
             for j in range(ybegin,yend + 1):
                 if (self.map[i][j][0] == 205 or self.map[i][j][0] == 0):
-                    self.map[waypoint.x][waypoint.y] = [255,0,0] #DEBUG mark invalid waypoints
+                    self.map[waypoint.x][waypoint.y] = [255,0,0]
                     return False
         return True
 
