c754045154f8ad93a39fab197453349035772d4d
sdudiak <staszek.dudiak@irpark.pl>
2022-04-22 15:14:33 +0200

    now launching entire app by run.sh

---
+++ mapper_packages/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+source "/opt/ros/galactic/setup.bash"
+source "/ros2_ws/install/setup.bash"
+
+ros2 run nav2_map_server map_saver_cli -f /map/map --ros-args -p save_map_timeout:=100.0
+echo "saving map..."
+sleep 3
+
+ros2 launch mappers_bringup mappers.launch.py
+exec "$@"
