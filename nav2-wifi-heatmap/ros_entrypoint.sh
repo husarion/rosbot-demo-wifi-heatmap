f337ab8254e7bde03429d52c217524fb9d15dfc4
sdudiak-husarion <stanislaw.dudiak@husarion.com>
2022-07-15 13:21:25 +0200

    change docker setup according to new standards

--- nav2-wifi-heatmap/ros_entrypoint.sh
+++ nav2-wifi-heatmap/ros_entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
-# setup Fast DDS RMW
-source /fastdds_overlay/install/setup.bash
-
 source "/ros2_ws/install/setup.bash"
 
 exec "$@"
